8ebfd767e92003da4956b15a9b73d723cd7de39d
take_cho <take.chup@gmail.com>
2018-05-29 08:40:01 +0900

    Applicationの作成

---
+++ Assets/App/Application.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Take.App
+{
+	public class Application : MonoBehaviour 
+	{
+		public static Application Instance()
+		{
+			return sInstance;
+		}
+
+		/// シーン読み込み時に必ず呼ばれる
+		[RuntimeInitializeOnLoadMethod(RuntimeInitializeLoadType.BeforeSceneLoad)]
+		static void OnLoad()
+		{
+			GameObject app = Instantiate(Resources.Load("Prefabs/App/Application")) as GameObject;
+			DontDestroyOnLoad(app);
+			app.name = "App";
+		}
+
+		/// アクセサ
+		// public SampleManager SampleManager()
+		// {
+		// 	return mSampleManager;
+		// }
+
+		public Save.SaveManager SaveManager()
+		{
+			return mSaveManager;
+		}
+
+		void Awake()
+		{
+			mSaveManager = new Save.SaveManager();
+			mSaveManager.Initialize();
+		}
+
+		// Update is called once per frame
+		void Update () 
+		{
+			
+		}
+
+		/// 各種データ
+		static Application sInstance;
+
+		/// セーブ
+		Save.SaveManager mSaveManager;
+	}
+}
---
+++ Assets/Save/SaveManager.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Take.Save
+{
+	public class SaveManager {
+
+		public SaveManager()
+		{
+		}
+
+		public void Initialize()
+		{
+
+		}
+
+	}
+}
